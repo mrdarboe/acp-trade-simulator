de6e17cb7ccc02509aea1729a382516b78df50f1
Gibril Darboe <gsdarboe1@gmail.com>
2022-04-15 07:53:39 +0000

    New platfotm frame

--- index.php
+++ index.php
@@ -1,53 +1,272 @@
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>ACP - Traders Training Platform</title>
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="description" content="HTTrack is an easy-to-use website mirror utility. It allows you to download a World Wide website from the Internet to a local directory,building recursively all structures, getting html, images, and other files from the server to your computer. Links are rebuiltrelatively so that you can freely browse to the local site (works with any browser). You can mirror several sites together so that you can jump from one toanother. You can, also, update an existing mirror site, or resume an interrupted download. The robot is fully configurable, with an integrated help" />
-	<meta name="keywords" content="httrack, HTTRACK, HTTrack, winhttrack, WINHTTRACK, WinHTTrack, offline browser, web mirror utility, aspirateur web, surf offline, web capture, www mirror utility, browse offline, local  site builder, website mirroring, aspirateur www, internet grabber, capture de site web, internet tool, hors connexion, unix, dos, windows 95, windows 98, solaris, ibm580, AIX 4.0, HTS, HTGet, web aspirator, web aspirateur, libre, GPL, GNU, free software" />
-	<meta name="generator" content="ACP">
-	<title>ACP Simulator</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<!-- Mirror and index made by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
-	<style type="text/css">
-		body{
-			background: rgb(253,193,45);
-			background: linear-gradient(0deg, rgba(253,193,45,1) 0%, rgba(34,193,195,1) 51%);
-		}
-		a, a:hover{
-			color: white;
-		}
-		.rem4{
-			font-size: 4rem;
-		}
-		.rem3{
-			font-size: 3rem;
-		}
-		.rem2{
-			font-size: 2rem;
-		}
-	</style>
+    <!-- Bootstrap -->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <!-- CSS -->
+    <link href="styles.css" rel="stylesheet">
+  </head>
 
-</head>
+  <body>
 
-<body>
+<div class="container py-3">
+  <header>
+    <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
+        <span class="fs-4">Alchemy Capital Partners</span>
+      </a>
 
-<div class="container-fluid text-center pt-5 text-white">
-  <h1 class="rem4">Alchemy Capital Partners</h1>
-  <h4 class="rem3">Trade Simulator</h4>
-  	<div class="d-grid gap-4 mt-5">
-			<a class="btn btn-primary rem2" HREF="gamezone.io/index.html">
-				GameZone
-			</a>
-			<a class="btn btn-primary rem2" HREF="velaspad.io/index.html">
-				VelasPad
-			</a>
-	</div>
+      <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+        <a class="me-3 py-2 text-light text-decoration-none btn btn-primary" href="#"><i class="bi bi-discord"></i> Discord</a>
+        <a class="py-2 text-light text-decoration-none btn btn-success" href="#"><i class="bi bi-calendar-week"></i> View Schedules</a>
+      </nav>
+    </div>
+
+    <div class="title-header p-3 pb-md-4 mx-auto text-center">
+      <h1 class="display-4 fw-normal">Traders Training Platform</h1>
+      <p class="fs-5 text-muted">Below are resources for various IDO Launchpads</p>
+    </div>
+  </header>
+
+  <main>
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">GameZone</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/gamezone.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="gamezone.io/index.html" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">BSCPad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/bscpad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">VelasPad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/velaspad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="velaspad.io/index.html" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">NFTLaunch</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/nftlaunch.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">MetaVPad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/metavpad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">ETHPad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/ethpad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">PulsePad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/pulsepad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">ADAPad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/adapad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">GameFI</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/gamefi.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">PolyPad</h4> <!--Pad Title-->
+          </div>
+          <div class="card-header py-3">
+            <img class="card-img-top" src="assets/media/polypad.png" alt=""><!--Pad Img-->
+          </div>
+          <div class="card-body">
+            <div class="mt-3 mb-4">
+              <a href="#" class="card-link">Watch Video</a>
+              <a href="#" class="card-link">Document</a>
+            </div>
+            <a class="w-100 btn btn-lg btn-outline-primary" href="#" role="button">Live Practice</a><!--Pad Link-->
+          </div>
+        </div>
+      </div>
+
+      
+    </div>
+
+    <h2 class="display-6 text-center mb-4">Upcoming Projects</h2>
+
+    <div class="embed-responsive embed-responsive-16by9 text-center">
+      <iframe class="embed-responsive-item" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRmbSeE1hQAHMYSBx1Uv1TAKT2C1acgjoPQxuiyJKde-G6Ui2z33YeAFRd-D4hu5DrMuZxjAi4D9aA7/pubhtml?widget=true&amp;headers=false"></iframe>
+    </div>
+
+  </main>
+
+  <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <small class="d-block mb-3 text-muted">ACP</small>
+      </div>
+
+      <div class="col-sm-6 col-md-4">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">ACP Website</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Join the Discord</a></li>
+        </ul>
+      </div>
+      
+      <div class="col-sm-6 col-md-4">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Guide to Crypto</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Some Podcast</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Some Article</a></li>
+        </ul>
+      </div>
+
+      <div class="col-md-4">
+        <h5>FAQs</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">What are IDOs?</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">What is the blockchain?</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">What is MetaMask?</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">What are public and private keys?</a></li>
+        </ul>
+      </div>
+
+    </div>
+  </footer>
 </div>
 
-	
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
 
+    
+  </body>
 </html>
-
-
